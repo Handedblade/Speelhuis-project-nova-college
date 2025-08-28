5746b389434bb5a79f068e30a91edbbf1605f196
rbeer008 <rbeer5655@gmail.com>
2025-08-28 16:43:39 +0200

    finished the brand class

---
+++ Classes/brands.php
@@ -0,0 +1,36 @@
+<?php
+
+class Brand
+{
+    public int $id;
+    public string $name;
+    public string $logo;
+
+    public static function findById($id)
+    {
+        $conn = new Database();
+        $conn->start();
+
+        $id = mysqli_real_escape_string($conn->connection, $id);
+
+        $sql = "SELECT * FROM brands WHERE brand_id = '$id'";
+        $result = $conn->connection->query($sql);
+
+        $brand = null;
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $brand = new Brand;
+                $brand->id = $row['brand_id'];
+                $brand->name = $row['brand_name'];
+                $brand->logo = $row['brand_logo'];
+            }
+        }
+
+        $conn->close();
+
+        return $brand;
+    }
+}
+
+?>
