55cd5f15d88f231cb71f4dfe90eba5e23262b27b
rbeer008 <rbeer5655@gmail.com>
2025-08-28 20:28:14 +0200

    the finished themes class

---
+++ Classes/themes.php
@@ -0,0 +1,34 @@
+<?php
+
+class Theme
+{
+    public int $id;
+    public string $name;
+
+    public static function findById($id) //finds a product's theme and returns it if found
+    {
+        $conn = new Database();
+        $conn->start();
+
+        $id = mysqli_real_escape_string($conn->connection, $id);
+
+        $sql = "SELECT * FROM themes WHERE theme_id = '$id'";
+        $result = $conn->connection->query($sql);
+
+        $theme = null;
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $theme = new Brand();
+                $theme->id = $row['theme_id'];
+                $theme->name = $row['theme_name'];
+            }
+        }
+
+        $conn->close();
+
+        return $theme;
+    }
+}
+
+?>
