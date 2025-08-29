92f3b5c40dcbbf87afd54e93056ea21b8ca45371
rbeer008 <rbeer5655@gmail.com>
2025-08-29 12:41:54 +0200

    changed index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include "Classes/themes.php";
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Speel Huis</title>
 </head>
