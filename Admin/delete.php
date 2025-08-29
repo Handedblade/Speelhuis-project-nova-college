e48afc171edf7fa819e49c68f0f155baf9bcf375
rbeer008 <rbeer5655@gmail.com>
2025-08-29 10:37:18 +0200

    added the file for deleting sets

---
+++ Admin/delete.php
@@ -0,0 +1,11 @@
+<?php
+
+//this page should only be accessed by logged in users with the "admin" role
+
+//this page deletes the selected set and then returns to the adminOwnerPage.php
+include "../Classes/sets.php";
+include "../Classes/database.php";
+include "../Classes/users.php";
+include "../Classes/sessions.php"
+
+?>
