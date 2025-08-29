d37ff9ba370af46145d033c4c8aa51cfc838a00d
rbeer008 <rbeer5655@gmail.com>
2025-08-29 10:30:18 +0200

    added the file for the admin owner page

---
+++ Admin/adminOwnerPage.php
@@ -0,0 +1,11 @@
+<?php
+
+//this page should only be accessed by logged in users with the "admin" role
+include "../Classes/users";
+include "../Classes/database";
+include "../Classes/brands";
+include "../Classes/sessions";
+include "../Classes/themes";
+include "../Classes/sets";
+
+?>
