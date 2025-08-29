7a9ab668a88fecd343e70d57353ecdc29d338c7b
rbeer008 <rbeer5655@gmail.com>
2025-08-29 10:37:09 +0200

    edited the adminWorkerPage

--- Admin/adminWorkerPage.php
+++ Admin/adminWorkerPage.php
@@ -1,11 +1,11 @@
 <?php
 
 //this page should only be accessed by logged in users with the "employee" role
-include "../Classes/users";
-include "../Classes/database";
-include "../Classes/brands";
-include "../Classes/sessions";
-include "../Classes/themes";
-include "../Classes/sets";
+include "../Classes/users.php";
+include "../Classes/database.php";
+include "../Classes/brands.php";
+include "../Classes/sessions.php";
+include "../Classes/themes.php";
+include "../Classes/sets.php";
 
 ?>
