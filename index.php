dfb14de8df2e9ea6a9fc134781891b59f0516ffe
rbeer008 <rbeer5655@gmail.com>
2025-08-29 10:24:30 +0200

    adding the file for the main page

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+//this page is for showing a brief overview of all the sets/products, no login required
+include "Classes/database.php";
+include "Classes/sets.php";
+include "Classes/brands.php";
+include "Classes/themes.php";
+
+?>
