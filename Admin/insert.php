2febb34915972d25847ca37a0ccbe68a29a01ee9
rbeer008 <rbeer5655@gmail.com>
2025-08-29 10:37:39 +0200

    added the file for adding new files

---
+++ Admin/insert.php
@@ -0,0 +1,12 @@
+<?php
+
+//this page should only be accessed by logged in users
+
+//this page allows for adding a new set and then returns to the 
+//adminWorker/adminOwner page depending on the users role
+include "../Classes/sets.php";
+include "../Classes/database.php";
+include "../Classes/users.php";
+include "../Classes/sessions.php"
+
+?>
