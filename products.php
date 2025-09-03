b56f41fb643cc80363d8f710a4a5e87a3d0ffa31
rbeer008 <rbeer5655@gmail.com>
2025-09-03 15:34:29 +0200

    added table with all products

--- products.php
+++ products.php
@@ -1,27 +1,54 @@
-   
-<?php
+   <?php
 
-include "Classes/database.php";
-include "Classes/sets.php";
-include "Classes/brands.php";
-include "Classes/themes.php";
+    include "Classes/database.php";
+    include "Classes/sets.php";
+    include "Classes/brands.php";
+    include "Classes/themes.php";
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
-    <title>Speel Huis</title>
-</head>
-<body>
-    
-    <nav class="navbar" aria-label="Main navigation">
-        <a href="#home">Speel Huis</a>
-        <a href="products.php">Products</a>
-        <a href="#contact">Contact</a>
-        <a href="Admin/index.php">Login</a>
-    </nav>
-</body>
-</html>
+    $sets = Set::findAll();
+
+    ?>
+   <!DOCTYPE html>
+   <html lang="en">
+
+   <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" href="css/index.css">
+       <title>Speel Huis</title>
+   </head>
+
+   <body>
+
+       <nav class="navbar" aria-label="Main navigation">
+           <a href="index.php">Speel Huis</a>
+           <a href="products.php">Products</a>
+           <a href="contact.php">Contact</a>
+           <a href="Admin/index.php">Login</a>
+       </nav>
+
+       <table border='1'>
+           <thead>
+               <th>Products</th>
+               <th>Name</th>
+               <th>Price</th>
+               <th></th>
+           </thead>
+           <tbody>
+               <?php
+
+                foreach ($sets as $set) { 
+                    $brand = Brand::findById($set->brandId)
+                    ?>
+                   <tr>
+                       <td><img src="images/logos/<?= $brand->logo; ?>" width="75px"></td>
+                       <td><?= $set->name ?></td>
+                       <td><?= $set->price ?></td>
+                       <td><a href="detail.php?id=<?= $set->id ?>">View Product</a></td>
+                   </tr>
+               <?php } ?>
+           </tbody>
+       </table>
+   </body>
+
+   </html>
