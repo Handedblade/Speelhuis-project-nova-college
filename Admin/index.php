4510bafdeeab5e096206e36c2ca673495e377693
rbeer008 <rbeer5655@gmail.com>
2025-09-03 10:41:32 +0200

    index is the login page

--- Admin/index.php
+++ Admin/index.php
@@ -6,3 +6,20 @@ include "../Classes/users.php";
 include "../Classes/sessions.php";
 
 ?>
+
+
+<!DOCTYPE html> 
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/styles.css">
+    <title>Login</title>
+</head>
+<body>
+    <form action="create.php" method="POST">
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
