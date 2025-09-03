1d5d2405e56e076cd35725ec98d7e775b09b79a1
mohtaha8 <mohammedray06@gmail.com>
2025-09-03 11:12:48 +0200

    Hier komen de producten

---
+++ products.php
@@ -0,0 +1,27 @@
+   
+<?php
+
+include "Classes/database.php";
+include "Classes/sets.php";
+include "Classes/brands.php";
+include "Classes/themes.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Speel Huis</title>
+</head>
+<body>
+    
+    <nav class="navbar" aria-label="Main navigation">
+        <a href="#home">Speel Huis</a>
+        <a href="products.php">Products</a>
+        <a href="#contact">Contact</a>
+        <a href="Admin/index.php">Login</a>
+    </nav>
+</body>
+</html>
