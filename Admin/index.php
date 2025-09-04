2b6bc38847f3f5bcad0135802f11c4b89e239a44
Handedblade <nathan.dewitt2007@gmail.com>
2025-09-04 13:56:18 +0200

    inlog pagina veranderd

--- Admin/index.php
+++ Admin/index.php
@@ -48,3 +48,54 @@ include "../Classes/users.php";
     ?>
 </body>
 </html>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login Pagina</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body class="bg-light d-flex align-items-center" style="height: 100vh;">
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+
+        <div class="card shadow-lg">
+          <div class="card-body">
+            <h3 class="text-center mb-4">Log in</h3>
+
+            <form name="stad" method="post" action="create.php">
+              <div class="mb-3">
+                <label class="form-label">Gebruikersnaam</label>
+                <input type="text" class="form-control" name="naam" value="" />
+              </div>
+
+              <div class="mb-3">
+                <label class="form-label">Wachtwoord</label>
+                <input type="text" class="form-control" name="ww" value="" />
+              </div>
+
+              <div class="d-grid">
+                <button type="submit" class="btn btn-primary btn-lg">Inloggen</button>
+              </div>
+            </form>
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
